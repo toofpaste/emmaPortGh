6480ac0dbf901aadf6a5aa668af3245d97c4f171
Michael Larragueta <mjlarragueta@gmail.com>
2020-02-28 09:56:08 -0800

    gh pages test

---
+++ index.js
@@ -0,0 +1,110 @@
+// $(document).ready(function() {
+//     const gif = './img/portfolio-eblm1.gif';
+//     const gif1 = './img/portfolio-eblm2.gif';
+//     const gif2 = './img/portfolio-expensive.gif';
+//     const gif3 = './img/portfolio-letugo1.gif';
+//     const gif5 = './img/portfolio-mad2.gif';
+//     const gif6 = './img/portfolio_gyw.gif';
+//     const gif7 = './img/portfolio_language1.gif';
+//     const gif8 = './img/portfolio_language2.gif';
+//     const gif9 = './img/portfolio_mad3.gif';
+//     const gif10 = './img/portfolio_mad4.gif';
+//     const gif11 = './img/portfolio_pickitup1.gif';
+//     const gif12 = './img/portfolio_pickitup2.gif';
+//     const gif13 = './img/caelus1.gif';
+//     const gif14 = './img/caelus3.gif';
+//     const gif15 = './img/portfolio_shake3.gif';
+//     const gif16 = './img/jasonoakes.gif';
+//     const gif17 = './img/caelus4.gif';
+//     const gif18 = './img/k2creative.gif';
+//     const gif19 = './img/AMRLLCLOGO-01.png';
+//     const gif20 = './img/caelus1 copy.png';
+//     const gif21 = './img/ICONS copy.png';
+//     const gif22 = './img/LOGOS2 copy.png';
+//     const gif23 = './img/Screen Shot 2017-02-13 at 12.59.33 AM.png';
+//     const gif24 = './img/Screen Shot 2017-10-16 at 12.36.09 AM.png';
+//     const gif25 = './img/Screen Shot 2017-10-16 at 12.38.49 AM.png';
+//     const gif26 = './img/SOGNOBADGE copy.png';
+//     const gif27 = './img/SOGNOVISUALS3 copy.png';
+//     const title = './img/emmaclizbe_1.png';
+//     const gif28 = './img/4 key components copy.png';
+//     const gif29 = './img/SHADTWITFINAL copy.png';
+//
+//     const ele = ("test");
+//     const ele1 = ("test1");
+//     const ele2 = ("test2");
+//     const ele3 = ("test3");
+//     const ele4 = ("test4");
+//     const ele5 = ("test5");
+//     const ele6 = ("test6");
+//     const ele7 = ("test7");
+//     const ele8 = ("test8");
+//     const ele9 = ("test9");
+//     const ele10 = ("test10");
+//     const ele11 = ("test11");
+//     const ele12 = ("test12");
+//     const ele13 = ("test13");
+//     const ele14 = ("test14");
+//     const ele15 = ("test15");
+//     const ele16 = ("test16");
+//     const ele17 = ("test17");
+//     const ele18 = ("test18");
+//     const ele19 = ("test19");
+//     const ele20 = ("test20");
+//     const ele21 = ("test21");
+//     const ele22 = ("test22");
+//     const ele23 = ("test23");
+//     const ele24 = ("test24");
+//     const ele25 = ("test25");
+//     const ele26 = ("test26");
+//
+//     const ele27 = ("test27");
+//     const ele28 = ("test28");
+//     const eleTitle = ("titleImage");
+//
+//
+//     const pd = './img/RESUME.pdf';
+//     const elep = ('resume');
+//     elep.href = pd;
+//
+//     $(`${eleTitle}`).attr('src', title);
+//     $(ele).attr('src', gif5);
+//     $(ele1).attr('src', gif9);
+//     $(ele2).attr('src', gif10);
+//
+//     $(ele3).attrr('src', gif7);
+//     $(ele4).attr('src', gif8);
+//     $(ele5).attr('src', gif2);
+//
+//     $(ele6).attr('src', gif);
+//     $(ele7).attr('src', gif3);
+//     $(ele8).attr('src', gif1);
+//
+//     $(ele9).attr('src', gif11);
+//     $(ele10).attr('src', gif15);
+//     $(ele11).attr('src', gif12);
+//
+//     $(ele12).attr('src', gif16);
+//     $(ele13).attr('src', gif17);
+//     $(ele14).attr('src', gif6);
+//
+//     $(ele15).attr('src', gif13);
+//     $(ele16).attr('src', gif18);
+//     $(ele17).attr('src', gif14);
+//
+//
+//     $(ele18).attr('src', gif19);
+//     $(ele19).attr('src', gif20);
+//     $(ele20).attr('src', gif21);
+//
+//     $(ele21).attr('src', gif22);
+//     $(ele22).attr('src', gif23);
+//     $(ele23).attr('src', gif24);
+//
+//     $(ele24).attr('src', gif25);
+//     $(ele25).attr('src', gif26);
+//     $(ele26).attr('src', gif27);
+//
+//     $(ele27).attr('src', gif28);
+//     $(ele28).attr('src', gif29);
+// }

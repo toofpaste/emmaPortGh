3cbb63433dc9bd47325d943a8c75c2f3cfeb71af
Michael Larragueta <mjlarragueta@gmail.com>
2020-03-04 00:03:02 -0800

    add more projects make tweaks

--- index.js
+++ index.js
@@ -7,8 +7,20 @@ $(window).scroll(function() {
     }
 });
 $(document).ready(function() {
+    $(".allgrid").fadeOut(10);
+    $("#top").fadeOut(10);
+    $("#hoverFor").fadeOut(10);
     $("#button").on('click', function (e) {
         e.preventDefault();
         $('html, body').animate({scrollTop: 0}, '300');
     });
+
+    setTimeout(function(){
+        $("#topLogo").fadeOut(1000);
+    }, 2500);
+    setTimeout( function () {
+        $(".allgrid").fadeIn(1000);
+        $("#top").fadeIn(1000);
+        $("#hoverFor").fadeIn(1000);
+    }, 3500);
 });
